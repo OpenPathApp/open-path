a0b353b902befa5de90b0d90969e1747261c6eff
tahsinamio <tahsinamio@gmail.com>
2023-05-13 15:14:18 -0700

    add filter component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import { Header } from '../../components/Header'
+import { Filter } from '../../components/Filter'
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <Filter />
     </main>
   )
 }
