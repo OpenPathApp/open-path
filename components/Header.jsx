a8cded4b3e6e2ea285b3bc9ba236fe01098d25b1
tahsinamio <tahsinamio@gmail.com>
2023-05-13 14:38:33 -0700

    modify header

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export const Header = () => {
     return (
-        <header className="flex justify-start items-center p-4 bg-neutral-900 shadow-md">
+        <header className="flex justify-between items-center p-4 bg-neutral-900 shadow-md">
             <div className="flex items-center px-4">
                 <Image
                     src="/rainbow.png" 
@@ -13,7 +13,7 @@ export const Header = () => {
                 />
             </div>
 
-            <div className="relative text-gray-600 w-2/5">
+            <div className="absolute pl-20 text-gray-600 w-2/5">
                 <input
                     className="border-2 border-gray-600 text-gray-400 bg-black h-10 px-5 pr-10 w-full rounded-lg text-sm focus:outline-none"
                     type="search"
@@ -25,6 +25,11 @@ export const Header = () => {
                         <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92c0.779,0,1.518-0.297,2.079-0.837  C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17s-17-7.626-17-17S14.61,6,23.984,6z"/>
                     </svg>
                 </button>
+            </div>
+
+            <div className="text-gray-400">
+                <span className="mx-4">About</span>
+                <span className="mx-4">Sign In</span>
             </div>
         </header>
     );
