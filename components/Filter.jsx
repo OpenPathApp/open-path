245ccef57ab7946ce03bc8fa015db0d9f81ad8e8
tahsinamio <tahsinamio@gmail.com>
2023-05-13 17:54:25 -0700

    add favicon

--- components/Filter.jsx
+++ components/Filter.jsx
@@ -5,17 +5,17 @@ export const Filter = () => {
     <div className="flex justify-around items-center p-4 bg-neutral-900 rounded-md shadow-md mt-10 mx-auto w-2/5">
       <label className="flex items-center">
         <input type="checkbox" className="form-checkbox h-5 w-5 text-blue-750" />
-        <span className="ml-2 text-gray-400">Hotels</span>
+        <span className="ml-5 text-yellow-400 font-bold">Hotels</span>
       </label>
 
       <label className="flex items-center">
         <input type="checkbox" className="form-checkbox h-5 w-5 text-blue-750" />
-        <span className="ml-2 text-gray-400">Restaurants</span>
+        <span className="ml-5 text-green-400 font-bold">Restaurants</span>
       </label>
 
       <label className="flex items-center">
         <input type="checkbox" className="form-checkbox h-5 w-5 text-blue-750" />
-        <span className="ml-2 text-gray-400">Restrooms</span>
+        <span className="ml-5 text-red-400 font-bold">Restrooms</span>
       </label>
     </div>
   );
