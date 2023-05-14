33ddbd7c44e37707b29451add54e9b8bfb2bbbc0
tahsinamio <tahsinamio@gmail.com>
2023-05-13 18:30:39 -0700

    fix map styling

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,25 +1,24 @@
 
 import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 import { useMemo } from "react";
-import './Map.css'
 
 export const Map = () => {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: 'AIzaSyAP6ZI6gP5_EmAu8md6W8uXBNM3eEXqx_A',
   });
-  const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
+  const center = useMemo(() => ({ lat: 40.730610, lng: -73.935242 }), []);
 
   return (
-    <div className="Map">
+    <div className="h-[48rem] w-3/4 m-auto rounded-lg mt-10 overflow-hidden">
       {!isLoaded ? (
         <h1>Loading...</h1>
       ) : (
         <GoogleMap
-          mapContainerClassName="map-container"
+          mapContainerClassName="h-3/4 w-full"
           center={center}
-          zoom={10}
+          zoom={14}
         >
-          <Marker position={{ lat: 18.52043, lng: 73.856743 }} />
+          <Marker position={{ lat: 40.730610, lng: -73.935242 }} />
         </GoogleMap>
       )}
     </div>
